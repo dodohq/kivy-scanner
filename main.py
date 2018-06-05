d7663d6225daaa7069913bdfcb097c5b11794aa0
pammelang <poppyfights@gmail.com>
2018-06-05 21:40:04 +0800

    Added pyzbar funcs

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ sm.add_widget(MainScreen(name='main'))
 sm.add_widget(LoadScreen(name='load'))
 sm.add_widget(UnlockScreen(name='unlock'))
 
+
+class Scanner(Widget):
+
+   def on_touch_down(self, touch):
+       if self.collide_point(*touch.pos):
+           self.pressed = touch.pos
+           # we consumed the touch. return False here to propagate
+           # the touch further to the children.
+           return True
+       return super(CustomBtn, self).on_touch_down(touch)
+
+   def on_pressed(self, instance, pos):
+       print ('pressed at {pos}'.format(pos=pos))
+
 class MainApp(App):
     def build(self):
         return sm
--- scanner.py
+++ scanner.py
@@ -5,53 +5,50 @@ import cv2
 import imutils 
 from imutils.video import WebcamVideoStream
  
-def decode(im) : 
-  # Find barcodes and QR codes
-  decodedObjects = pyzbar.decode(im)
- 
-  # Print results
-  for obj in decodedObjects:
-    print('Type : ', obj.type)
-    print('Data : ', obj.data,'\n')
-    print('Points : ', obj.polygon,'\n')
-  return decodedObjects
- 
- 
-# Display barcode and QR code location  
-def display(im, decodedObjects):
- 
-  # Loop over all decoded objects
-  for decodedObject in decodedObjects: 
-    points = decodedObject.polygon  
-    # If the points do not form a quad, find convex hull
-    if len(points) == 4 : 
-      print('top left: ', points[1].x, ' ', points[1].y)
-      print('bottom right: ', points[3].x, ' ', points[3].y)
-      cv2.rectangle(im, (points[0].x, points[0].y), (points[2].x, points[2].y), (0,255,0), 3)
+class Scanner():
+  def __init__(self, src=1):
+    self.src = src
+    self.code = ''
     
-  # Display results 
-  cv2.imshow("Results", im);
-  cv2.waitKey(1);
- 
-   
-# Main 
-if __name__ == '__main__':
- 
-  # Read image
-  # im = cv2.imread('zbar-test.jpg')
-
-  camera = WebcamVideoStream(src=1).start()
+  def __decode(im): 
+    # Find barcodes and QR codes
+    decodedObjects = pyzbar.decode(im)
+    # Print results
+    for obj in decodedObjects:
+      print('Type : ', obj.type)
+      print('Data : ', obj.data,'\n')
+      print('Points : ', obj.polygon,'\n')
+      self.code = obj.data
+    return decodedObjects
   
+  # Display barcode and QR code location  
+  def display(im, decodedObjects):
+    # Loop over all decoded objects
+    for decodedObject in decodedObjects: 
+      points = decodedObject.polygon  
+      if len(points) == 4 : 
+        cv2.rectangle(im, (points[0].x, points[0].y), (points[2].x, points[2].y), (0,255,0), 3)
+    # Display results 
+    cv2.imshow("Results", im)
+    cv2.waitKey(1)
 
-  while(True): 
-    frame = camera.read()
-	  # resize the frame and convert it to grayscale (while still
-	  # retaining 3 channels)
-    frame = imutils.resize(frame, width=400)
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #frame = np.dstack([frame, frame, frame])    
-    decodedObjects = decode(frame)
-    display(frame, decodedObjects)
+  def decoded(self):
+    if not self.code:
+      return False
+    else return self.code
+ 
+  def call(self):
+    camera = WebcamVideoStream(src=self.src).start()
+    while(True): 
+      frame = camera.read()
+      # resize the frame and convert it to grayscale (while still
+      # retaining 3 channels)
+      frame = imutils.resize(frame, width=400)
+      #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+      #frame = np.dstack([frame, frame, frame])    
+      decodedObjects = decode(frame)
+      display(frame, decodedObjects)
 	    
-  cv2.destroyAllWindows()
-  camera.stop()
+    cv2.destroyAllWindows()
+    camera.stop()
+
