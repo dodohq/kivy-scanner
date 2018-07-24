dc756d8f2aa7ab820e6e0b4958a0f25439f8c864
pammelang <poppyfights@gmail.com>
2018-07-24 22:06:17 +0800

    Init permissions for gps in boot file

--- app/boot_up.sh
+++ app/boot_up.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+systemctl stop gpsd.socket
+systemctl disable gpsd.socket
+sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 python3 /home/nvidia/Desktop/scanner_gui/app/init_gpio.py
 chmod 777 /dev/input/event7
 python3 /home/nvidia/Desktop/scanner_gui/app/main.py
